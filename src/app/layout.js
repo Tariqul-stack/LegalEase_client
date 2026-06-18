16ce378d205c1609aea7f5f77005268d2d24c89f
Tariqul911 <zxtariqul911@gmail.com>
2026-06-18 19:24:37 +0800

    feat: add footer with quick links, social icons, and newsletter signup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import QueryProvider from "@/components/QueryProvider";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
           <main className="flex-1 bg-gray-50">
             {children}
           </main>
+          <Footer />
         </QueryProvider>
       </body>
     </html>
